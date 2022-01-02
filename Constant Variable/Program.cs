75a6869d4ed94429a52336af469ab7d7e9990420
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-01-02 13:48:23 +1300

    ConstantVariable

--- Constant Variable/Program.cs
+++ Constant Variable/Program.cs
@@ -8,9 +8,26 @@ namespace Constant_Variable
 {
     internal class Program
     {
+
+        //public const double PI = 3.14;
+        public const string Name = "Shubham";
         static void Main(string[] args)
         {
-            //Constant Variables
+            
+            Console.WriteLine(Name);
+            Console.ReadLine();
+            
+
+
+
+
+            //Constant Variables : these variables are the values that are unchanged when value is provided
+            // It can be declared in class and method.
+            // These can be used by applying access modifiers eg(Public, Private) in class.
+           
+
+            // *****Constant Variable's value  must Initialized while Declaration.
+            // It must be used by using Const keyword..
         }
     }
 }
