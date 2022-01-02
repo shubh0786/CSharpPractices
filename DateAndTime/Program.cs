c6298c79faf0c38bbf68ce4f947713b951435c99
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-01-02 14:24:40 +1300

    DateAndTimeFOrmating

--- DateAndTime/Program.cs
+++ DateAndTime/Program.cs
@@ -13,12 +13,27 @@ namespace DateAndTime
             // These are the date time formatters
 
             DateTime dt = DateTime.Now;
-            Console.WriteLine("{0:d}",dt);
-            Console.WriteLine("{0:D}", dt);
-            Console.WriteLine("{0:f}", dt);
-            Console.WriteLine("{0:F}", dt);
-            Console.WriteLine("{0:g}", dt);
-            Console.WriteLine("{0:G}", dt);
+            Console.WriteLine("{0:d}",dt);// For short Date only  eg: (2/01/2022)
+            Console.WriteLine("{0:D}", dt);// For Full Date only eg: (Sunday, 2 January 2022)
+            Console.WriteLine("{0:f}", dt);// For Short time but date will be default full eg: (Sunday, 2 January 2022 2:18 pm)
+            Console.WriteLine("{0:F}", dt);//For Full time with seconds but date will be default full eg: (Sunday, 2 January 2022 2:19:35 pm)
+            Console.WriteLine("{0:g}", dt);// For short date and time eg: (2/01/2022 2:20 pm)
+            Console.WriteLine("{0:G}", dt);// For short date and fulltime with seconds eg: (2/01/2022 2:20:57 pm)
+            Console.WriteLine("{0:m}", dt);
+            Console.WriteLine("{0:t}", dt);
+            Console.WriteLine("{0:T}", dt);
+            Console.WriteLine("{0:y}", dt);
+            Console.WriteLine("{0:yy}", dt);
+            Console.WriteLine("{0:yyyy}", dt);
+            Console.WriteLine("{0:ddd}", dt);
+            Console.WriteLine("{0:dddd}", dt);
+            Console.WriteLine("{0:HH}", dt);
+            Console.WriteLine("{0:MM}", dt);
+            Console.WriteLine("{0:MMM}", dt);
+            Console.WriteLine("{0:MMMM}", dt);
+            Console.WriteLine("{0:ss}", dt);
+
+            Console.WriteLine("{0:dd-MMM-yyyy: hhtt}", dt); // You can customize your Date format
 
             Console.ReadLine();
         }
