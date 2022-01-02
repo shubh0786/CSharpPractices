be85bf9030802ff0f0a70823cdbf53dee797ede2
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-01-02 13:59:23 +1300

    DateTime

---
+++ DateAndTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // These are the date time formatters
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("{0:d}",dt);
+            Console.WriteLine("{0:D}", dt);
+            Console.WriteLine("{0:f}", dt);
+            Console.WriteLine("{0:F}", dt);
+            Console.WriteLine("{0:g}", dt);
+            Console.WriteLine("{0:G}", dt);
+
+            Console.ReadLine();
+        }
+    }
+}
