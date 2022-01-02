14b6a704f8c7337d57f170f8348b52f4119d7eec
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-01-02 13:30:09 +1300

    Csharp

---
+++ Constant Variable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constant Variables
+        }
+    }
+}
--- DataType_conversions/Program.cs
+++ DataType_conversions/Program.cs
@@ -10,6 +10,35 @@ namespace DataType_conversions
     {
         static void Main(string[] args)
         {
+
+            //string a = "25";
+            //string b = "25";
+
+
+            //int c = int.Parse(a) + int.Parse(b);
+
+            //Console.WriteLine(c);
+            //Console.ReadLine();
+
+            string d = "25.898";
+            string e = "29.878";
+
+            float f = float.Parse(d) + float.Parse(e);
+            Console.WriteLine(f);
+            Console.ReadLine();
+
+            //Points to be noted there are two types of data conversions.
+            // **Implicit and **Explicit.
+
+            // ** Implicit Conversion: This is sytem applied convertion which took place
+            // When thier is no loss of data.
+
+            // ** Explicit: Whereas Explicit is implemented by a user
+            // so he knows he will be the responsible of any data loss.
+
+            // Parse conversion is only used when converting a string to any data type.
+
+
         }
     }
 }
