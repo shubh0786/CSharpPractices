bca6de9258529a33b4250d0e6a1b1d639865679e
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-01-04 13:01:39 +1300

    If else

---
+++ IfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write your second number");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+
+                Console.WriteLine("Both are equal");             
+            }
+            else
+            {
+                Console.WriteLine("They are not equal");
+            }
+             
+            Console.ReadLine();
+        }
+    }
+}
